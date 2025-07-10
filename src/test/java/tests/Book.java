7b739650d247fa1963ce1fc387ef08d80f6a30c1
AKumarVatsa <adarshvatsa7@gmail.com>
2025-07-10 10:49:11 +0530

    Book.java

---
+++ src/test/java/tests/Book.java
@@ -0,0 +1,19 @@
+package pojo;
+
+public class Book {
+    private String isbn;
+
+    public Book() {}
+
+    public Book(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+}
