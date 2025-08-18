1cec51679fcb4d4f361e7f0736f9bbba435bb3e5
AKumarVatsa <adarshvatsa7@gmail.com>
2025-08-18 22:11:09 +0530

    Update TestRunner.java

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,18 +1,14 @@
 package runner;
-
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        // point to your .feature file location
-        features = "src\\test\\java\\resource\\sample.feature",
-        		glue = {"step"},
-
-        plugin = {
-                "pretty",
-                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
-        },
-        monochrome = true
+    features = "src/test/java/resource",   // ✅ Correct location
+    glue = "step",                   
+    plugin = {
+        "pretty",
+        "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
+    }
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
 }
